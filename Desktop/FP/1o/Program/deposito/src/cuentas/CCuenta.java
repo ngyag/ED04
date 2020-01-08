6c699cbc8e1fef2288a32254b59aecdd03f743ee
15yag <15yag@DESKTOP-60I35VA>
2020-01-08 17:28:25 +0100

    .

---
+++ Desktop/FP/1o/Program/deposito/src/cuentas/CCuenta.java
@@ -0,0 +1,74 @@
+package cuentas;
+
+public class CCuenta {
+
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInter√s;
+
+    public CCuenta()
+    {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo)
+    {
+        setNombre(nom);
+        setCuenta(cue);
+        setSaldo(sal);
+    }
+
+    public double estado()
+    {
+        return getSaldo();
+    }
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private double getSaldo() {
+		return saldo;
+	}
+
+	private void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInter√s() {
+		return tipoInter√s;
+	}
+
+	public void setTipoInter√s(double tipoInter√s) {
+		this.tipoInter√s = tipoInter√s;
+	}
+}
